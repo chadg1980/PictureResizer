706359ac9ff24bf9d7207cd34bb34dae4de5a824
chad_h_glaser <chadg@healthslate.com>
2017-10-04 14:49:16 -0700

    fixed whitespace

--- index.js
+++ index.js
@@ -3,39 +3,34 @@ const gm = require('gm').subClass({imageMagick: true});
 exports.handler = (event, context, callback) => {
 
     console.log('Received event:', JSON.stringify(event, null, 2));
-    let inputBuffer = 'originalpics/picture006.jpg';
-    let dest = 'editedpics/cropPicture006.jpg'
+    let inputBuffer = 'originalpics/picture006.jpg'; 	//Change to the input picture
+    let dest = 'editedpics/cropPicture006.jpg'		//Change to the destination for the picture
     let width = height = 200;
     let largerDimen;
 
-    gm(inputBuffer).size((err, value)=>{
-    	if(value['height']>value['width']){
+	gm(inputBuffer).size((err, value)=>{
+	    	//Saving height value
+	    	if(value['height']>value['width']){
+	    		largerDimen = "Height is larger than width";
+	    		gm(inputBuffer)
+	    		.resize(null, 200)
+	    		.write(dest, function (err){
+	    			if(err) callback(err);
+	    			callback(null, "success");
+	    		});
+		//Saving width value	
+	    	}else{
+	    		largerDimen = "Width is larger than Height";
+	    		gm(inputBuffer)
+	    		.resize(200)
+	    		.write(dest,function (err){
+	    			if(err) callback(err);
+	    			callback(null, "success");
+	    		});
+	        }
+	    	callback(null, "Crop Successful");
+	});
     		
-    		largerDimen = value['height'];
-    		gm(inputBuffer)
-    		.resize(null, 200)
-    		.write(dest, function (err){
-    			if(err) callback(err);
-    			callback(null, "success");
-    		});	
-    	}else{
-    		largerDimen = value['width'];
-    		gm(inputBuffer)
-    		.resize(200)
-    		.write(dest,function (err){
-    			if(err) callback(err);
-    			callback(null, "success");
-    		});
-    	}
-    	callback(null, JSON.stringify(value) + ": " + largerDimen);
-    	
-
-    });
-    		
-  
-    
- 
-
 }
     
 
