a2e063e204a292898ccdad048231a284d155add3
chad_h_glaser <chadg@healthslate.com>
2017-11-03 15:17:22 -0700

    updated the client side data

--- front_end/javascript/fileupload.js
+++ front_end/javascript/fileupload.js
@@ -3,6 +3,7 @@
 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
 //curl --request POST https://gbb8xz2947.execute-api.us-east-1.amazonaws.com/example_test/headersresource -H "Content-Type: application/json" -H "header1: API Gateway and AWS Lambda" -X POST -d "{\"API_body\": \"This is the body\"}" 
 */
+//https://nodejs.org/api/http.html
 
 //try this
 //https://code.tutsplus.com/tutorials/uploading-files-with-ajax--net-21077
@@ -10,25 +11,44 @@
 $(document).ready(function(){
     $( '#uploadForm').submit(function( event ){
         event.preventDefault();
-        let form = $( this );
-        term = form.find( "input[name='coachid']").val();
-        console.log(term);
-                
+
+        
+        let myData = $('#uploadForm').serializeArray();
+        let fileData = document.getElementById("picfile").files;
+        if(fileData.length === 0 || fileData.length >= 2){
+            alert("must upload 1 image");
+        }
+
+        let fileDetail = {
+            "name" : fileData[0].name,
+            "size" : fileData[0].size,
+            "type" : fileData[0].type,
+        };
+        
+        //myData.push( {name:"filedata", value: fileData[0]} );
+        myData.push(fileDetail);
+        
+        console.log(myData);        
+        myData = JSON.stringify( myData ); 
+        console.log(myData);        
+                        
         $.ajax({
             url : 'https://gbb8xz2947.execute-api.us-east-1.amazonaws.com/example_test',
             Accept: "images/*", 
             type: 'post',
-            processData: false, 
+            processData: false,
+            data: myData, 
             headers: {
                'Content-Type': 'application/json', 
                'header1': 'API Gateway and AWS Lambda'
                
             },
-            contentType: "application/json", 
+            contentType: false, 
             
             dataType: 'json',
             success: function(data){
-                console.log("success  " + data);
+                $("#response").append("<p> uploaded picture for coach ID: " + data.value + "</p>");
+                console.log(data);
             }, 
             error: function(err){
                 console.log("error "+ err.error);
