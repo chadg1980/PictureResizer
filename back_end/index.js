8b6a942c38910fcbc00853e3b62998ecde90d340
chad_h_glaser <chadg@healthslate.com>
2018-01-16 15:58:37 -0800

    forced PNG on all uploaded images. added a promise chain

--- back_end/index.js
+++ back_end/index.js
@@ -4,20 +4,6 @@ const AWS = require('aws-sdk');
 const fileType = require('file-type');
 const s3 = new AWS.S3({apiVersion: '2006-03-01', region: 'us-east-1'});
 
-function getContent(ext){
-    if(ext == 'png' || ext == 'PNG'){
-        return 'image/png'
-    }
-    if(ext == 'jpg' || ext == 'JPG'){
-        return 'image/jpeg'   
-    }
-
-    console.log("ext unknown " + ext);
-    callback(null, "unknown file extention " + ext);
-}
-
-
-
 function getParams(image_name_ext, data, imageContentType){
 
     let params = {
@@ -28,10 +14,7 @@ function getParams(image_name_ext, data, imageContentType){
         
     };
     return params;
-
 }
-
-
 
 exports.handler = (event, context, callback) => {
     let this_id = -1;
@@ -41,72 +24,63 @@ exports.handler = (event, context, callback) => {
     let height = 200;
     let width = 200;
     let imageData;
-    let ext;
+    
     let fileName = "coachid_" +this_id; 
     let imageContentType;
     let image_name_ext
     delete req.operation;
-    /*
-    if (operation) {
-        console.log(`Operation ${operation} 'requested`);
+    
+    //Possible do a safety check and pull all valid coachID from the database to check against. 
+    if (this_id <= 0) {
+        console.log("invalid coachID");
+        callback("Invalid coachID");
     }
-    */
-
+  
+    //Only continue if their is a valid base64Image in the body. 
     if (!req.base64Image) {
         const msg = 'Invalid resize request: no "base64Image" field supplied';
         console.log(msg);
         return callback(msg);
     }
-
+    //Sharp will resize a base64 image to 200 x 200
+    //Output is forced to be PNG
+    //Output is a buffer. 
     sharp(new Buffer(req.base64Image, 'base64'))
     .resize(height, width)
-    .toBuffer(function(err, data, info){
-        if(err){
-            console.log(err.message);
-            callback(err.message);
-        }
-        
-        console.log("info");
-        console.log(info);
+    .png()
+    .toBuffer()
+    .then( data => {
+
         console.log("data")
         console.log(data);
+        image_name_ext = fileName+".png";
 
-        ext = info.format;
-        if(ext == 'jpeg' || ext == 'JPEG'){
-            ext = "jpg";
-        }
-        imageContentType = getContent(ext);
-        image_name_ext = fileName+"."+ext;
-        
-
+        //get the params for s3
         let file_params = getParams(image_name_ext, data, imageContentType);
+        var putObjectPromise = s3.putObject(file_params).promise();
+        putObjectPromise.then( data => {
 
-        s3.putObject(file_params, function (err, data){
-            if(err){
+            console.log("s3 data");
+            console.log(data);
+            console.log("successfully upladed the image");
+            callback(null, "https://s3.amazonaws.com/coachpic.healthlate.com/"+image_name_ext);
 
-                console.log("s3 error: " + err);
-<<<<<<< HEAD
-                    //console.log("Error uploading data: ", data);
-                    callback(null, err);
-                }else{
-                    console.log("s3 data");
-                    console.log(data);
-                    console.log("successfully upladed the image");
-                    callback(null, "https://s3.amazonaws.com/coachpic.healthlate.com/"+image_name_ext);
-                }
+        }).catch( err => {
+            console.log("s3 error: " + err);
 
-            });
-=======
-                //console.log("Error uploading data: ", data);
-                callback(null, err);
-            }else{
-                console.log("s3 data");
-                console.log(data);
-                console.log("successfully upladed the image");
-                callback(null, "https://s3.amazonaws.com/coachpic.healthlate.com/"+image_name_ext);
-            }
-
-        });
->>>>>>> refs/remotes/origin/master
+            //console.log("Error uploading data: ", data);
+            callback(null, err);
+        })
+            
+               
+        })
+    .catch(err => {
+        console.log("Error")
+        callback("Error");
     })
+        
+     
+
+       
+    
 };
